8d297fb8c267995e0777f8baadad9ea59b17d728
fedchenkoa <nsk.v.kadre@gmail.com>
2020-05-10 00:40:50 +0700

    Bottom sheet по 2 уроку

--- app/src/main/java/com/example/materialdesignapp/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/materialdesignapp/ui/slideshow/SlideshowFragment.java
@@ -1,33 +1,54 @@
 package com.example.materialdesignapp.ui.slideshow;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.materialdesignapp.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
+    private static final String TAG = "BottomActivity";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final View llBottomSheet = root.findViewById(R.id.bottom_sheet);
+        
+        // init the bottom sheet behavior
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // change the state of the bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // set the peek height
+//        bottomSheetBehavior.setPeekHeight(150);
+
+        // set hideable or not
+//        bottomSheetBehavior.setHideable(true);
+
+        // set callback for changes
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                Log.d(TAG, "onStateChanged: ");
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.d(TAG, "onSlide: ");
             }
         });
         return root;
