82e320cfd69fd41dbfe036d2ca41a917e29c88b0
fedchenkoa <nsk.v.kadre@gmail.com>
2020-05-09 22:59:59 +0700

    Edit Text и Snack Bar по 2 уроку

--- app/src/main/java/com/example/materialdesignapp/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/materialdesignapp/ui/gallery/GalleryFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.materialdesignapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class GalleryFragment extends Fragment {
 
@@ -28,6 +31,20 @@ public class GalleryFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        final FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Произвелось какое-то действие", Snackbar.LENGTH_LONG)
+                        .setAction("Действие", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(getContext(), "Бумс", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
             }
         });
         return root;
